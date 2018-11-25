e9e598015c49e209f032f7c74debf72f47354e01
track2 <track2@gmail.com>
2018-11-25 21:59:11 +0200

    Add files via upload

---
+++ reactdemo/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace reactdemo.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getRepos()
+        {
+            HttpContext.Session["repo"] = "some repo data";
+            return Json(HttpContext.Session["repo"], JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult addRepo(string repoResult)
+        {
+            HttpContext.Session["repo"] = repoResult;
+            return Json(repoResult, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
